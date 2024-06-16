9ed24760d9d7acda3f90fdbf2a1d15fc9586bd17
cyborg97 <muhametshin.1997@gmail.com>
2024-06-16 08:37:10 +0200

    feat: implement buy ticket

--- src/app/views/buy-ticket/styled.ts
+++ src/app/views/buy-ticket/styled.ts
@@ -1,29 +1,29 @@
 import styled from "styled-components";
 
 export const BuyTicketWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   flex-grow: 1;
 `;
 
 export const Ticket = styled.li`
-	display: flex;
-	flex-shrink: 0;
-	flex-direction: column;
-	gap: 5px;
-	width: 150px;
-	height: 200px;
-	justify-content: space-between;
-	align-items: center;
-	background: linear-gradient(to bottom right, #3a1c71, #d76d77, #ffaf7b);
-	color: white;
-	padding: 20px;
-	border-radius: 10px;
-	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-	user-select: none;
-	cursor: pointer;
+  display: flex;
+  flex-shrink: 0;
+  flex-direction: column;
+  gap: 5px;
+  width: 150px;
+  height: 200px;
+  justify-content: space-between;
+  align-items: center;
+  background: linear-gradient(to bottom right, #3a1c71, #d76d77, #ffaf7b);
+  color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  user-select: none;
+  cursor: pointer;
 `;
 
 export const Service = styled.span``;
@@ -31,24 +31,24 @@ export const Service = styled.span``;
 export const Zone = styled.span``;
 
 interface ITime {
-	size?: number;
+  size?: number;
 }
 export const Time = styled.span<ITime>`
-	font-size: ${(props) => props.size || 70}px;
-	font-weight: 500;
-	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  font-size: ${(props) => props.size ?? 70}px;
+  font-weight: 500;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
 
 export const Description = styled.span`
-	text-align: center;
+  text-align: center;
 `;
 
 export const Price = styled.span`
-	font-weight: 600;
-	& span {
-		font-weight: 400;
-		font-size: 12px;
-	}
+  font-weight: 600;
+  & span {
+    font-weight: 400;
+    font-size: 12px;
+  }
 `;
 
 export const StyledButton = styled.button`
@@ -86,4 +86,3 @@ export const StyledButton = styled.button`
     background-color: #a93226;
   }
 `;
-
